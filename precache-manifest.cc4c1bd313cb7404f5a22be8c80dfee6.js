89cad069d74af8dfda512c0020afcdee23671cdf
aa0965 <anshupratapsingh32391@gmail.com>
2020-05-13 05:32:39 +0530

    Updates

---
+++ precache-manifest.cc4c1bd313cb7404f5a22be8c80dfee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f33ec3ef373a1952e358102db6c7048",
+    "url": "/Trello-Board/index.html"
+  },
+  {
+    "revision": "cc0b896e72a6d208ac75",
+    "url": "/Trello-Board/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "be939cb5e87d80eedc8d",
+    "url": "/Trello-Board/static/js/2.8d5eddac.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Trello-Board/static/js/2.8d5eddac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0b896e72a6d208ac75",
+    "url": "/Trello-Board/static/js/main.89fc56a7.chunk.js"
+  },
+  {
+    "revision": "0753b1669d37ae58a682",
+    "url": "/Trello-Board/static/js/runtime-main.bedb8698.js"
+  }
+]);
